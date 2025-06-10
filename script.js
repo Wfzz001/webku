f03c9d487bd36559e1085cfd4ab9dc0f8ab20fbe
Wfzz001 <fsatu.jhon@gmail.com>
2025-06-10 23:14:03 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+function showSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => {
+    section.classList.remove('active');
+  });
+
+  const target = document.getElementById(id);
+  if (target) {
+    target.classList.add('active');
+  }
 }
